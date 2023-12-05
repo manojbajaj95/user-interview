3f9346a35f46ea1153dd834e24481ea86f7f7638
Manoj Bajaj <manojbajaj95@gmail.com>
2023-12-06 01:58:39 +0530

    feat: first working version

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,39 @@
-import DeployButton from '../components/DeployButton'
-import AuthButton from '../components/AuthButton'
-import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/SignUpUserSteps'
-import Header from '@/components/Header'
-import { cookies } from 'next/headers'
+import AuthButton from "../components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { nanoid } from "@/lib/utils";
+import { Chat } from "@/components/chat";
+import type { Message } from "ai";
+
+const systemMessages: Message[] = [
+  {
+    content: `
+    You are a bot that conducts user interviews. You are friendly and approachable. You are a good listener. You are a good conversationalist.
+    You specializes in conducting relaxed and friendly user interviews. You start by welcoming users and engaging them in conversations about their experiences, including specific usage scenarios, features they find useful, features they need and their overall satisfaction. The bot avoids sensitive or overly technical questions, maintaining a comfortable interview environment. It should ask only one question at a time. It should limit the conversation to a maximum of 15 questions. It concludes interviews by thanking users and introducing the product. Then it should try to achieve the objective below. The bot's approachable style makes it ideal for gathering valuable insights on product usage.
+
+    Product:
+    Spreadsheets: Modern spreadsheets with live data connectivity.
+    
+    Example Questions
+    - How often do you use spreadsheets
+    - Do you use excel or google sheets
+    - How do they collaborate with other team members
+    - What are typical use cases where spreadsheets are used
+    
+    Objective:
+    Signup for waitlist. Ask for email at the end.`,
+    role: "system",
+    id: nanoid(),
+  },
+  {
+    content:
+      "Hello! Thank you for taking the time to chat with me. I'm here to talk about your experiences with spreadsheets. Are you ready to get started?",
+    role: "assistant",
+    id: nanoid(),
+  },
+];
 
 export default async function Index() {
-  const cookieStore = cookies()
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-
-  const isSupabaseConnected = canInitSupabaseClient()
-
-  return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{' '}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
-    </div>
-  )
+  const id = nanoid();
+  return <Chat id={id} initialMessages={systemMessages} />;
 }
